934717bf63c4294e13464e9338aa21f2f892c379
U-DIR\kentaro.kakimoto <kentaro.kakimoto@CPX-65OI1CP9LHX.dir.svc.accenture.com>
2017-09-21 22:07:35 +0900

    from windows

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,22 @@
 class UsersController < ApplicationController
+
   def show
   	@user = User.find(params[:id])
   end
   
   def new
-  	@user = User.new
+  	@user = User.new(params[:user])
+  	if @user.save
+  		flash[:success] = "Welcome to 柿本勤怠システム"
+  	  	redirect_to @user
+  	else
+  	  	render 'new'
+  	end
   end
+
+  private 
+  def user_params
+    params.require(:user).permit(:enterprise_id,:name, :email, :password ,:password_comfirmation)
+  end
+
 end
